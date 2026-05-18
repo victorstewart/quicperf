98eb9aeb2b73ae6ff51cb0297d35d8e330a02fee
Victor Stewart <v@nametag.social>
2026-05-18 05:52:07 +0000

    Bootstrap depos 0.5.3

--- depos.project.cmake
+++ depos.project.cmake
@@ -1,2 +1,2 @@
-set(DEPOS_BOOTSTRAP_VERSION "0.5.2" CACHE STRING "Pinned depos version used by this project" FORCE)
+set(DEPOS_BOOTSTRAP_VERSION "0.5.3" CACHE STRING "Pinned depos version used by this project" FORCE)
 set(DEPOS_ALLOW_SYSTEM_EXECUTABLE OFF CACHE BOOL "Allow using a system depos executable when available" FORCE)
